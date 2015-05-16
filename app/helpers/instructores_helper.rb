1138c51a961c107ba9357ced3bcc299c7d061b7e
JuanHenao <juan-o-henao@hotmail.es>
2015-05-16 21:32:39 +0000

    show instructores color transparente

--- app/helpers/instructores_helper.rb
+++ app/helpers/instructores_helper.rb
@@ -1,4 +1,18 @@
 module InstructoresHelper
     
-    
+    def calidad (v1, v2)
+     	re = v1
+     	
+     	
+ 	end
+    def prom (v1, v2)
+     	re = v1
+     	
+     	
+ 	end
+ 	def sum (v1, v2)
+     	re = v1
+     	
+     	
+ 	end
 end
